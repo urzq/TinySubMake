5b5520b13d8427feed334779afd64a2185a9150f
urzq <clement.lenhof@gmail.com>
2018-02-08 23:53:58 +0100

    Add tinyxml2 to the build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <tinyxml2.h>
 
 int main(int argc, char const *argv[])
-{
+{    
+	tinyxml2::XMLDocument doc;
+
 	std::cout << "Hello TinySubMake\n";
 	return EXIT_SUCCESS;
 }
